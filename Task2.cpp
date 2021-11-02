bb4b42716610ff6db654ba0421618fd1bb4d93a5
Adarsh <adarshsahlot@gmail.com>
2021-11-02 21:49:34 +0530

    Task

--- Task2.cpp
+++ Task2.cpp
@@ -1,37 +1,88 @@
-#include "bits/stdc++.h"
+// Q - 1
+
+// #include <iostream>
+// #include <math.h>
+// using namespace std;
+// int main()
+// {
+//     int input, count = 0;
+//     cin >> input;
+//     int temp = input;
+
+//     while (temp > 0)
+//     {
+//         temp = temp / 10;
+//         count++;
+//     }
+
+//     int num = input * input;
+//     int temp1 = num, temp2 = num;
+
+//     int lastNum = temp1 % (int(ceil(pow(10, count))));
+//     int firstNum = temp2 / (int(ceil(pow(10, count))));
+
+//     if (firstNum + lastNum == input)
+//         cout << "Chef's Number";
+//     else
+//         cout << "Not Chef's Number";
+// }
+
+// Q-2
+
+#include <iostream>
 using namespace std;
-int countDigits(int num)
-{
-    int count = 0;
-    while (num != 0)
-    {
-        num = num / 10;
-        count++;
-    }
-    return count;
-}
+
 int main()
 {
-    int n, m;
-    double x, y;
-    cin >> n;
-    int org_n = n;
-    n = n * n;
-    int digits = countDigits(n);
-    if (digits % 2 == 0)
-    {
-        m = digits / 2;
-        x = n / pow(10, m);
-        y = n % pow(10, m);
+    int size;
+    cin >> size;
+    int arr[size];
 
-        if (x + y == org_n)
+    int i, temp, key;
+
+    for (i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cin >> key;
+
+    for (i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
         {
-            cout << "Chef Number";
-        }
-        else
-        {
-            cout << "Not Chef Number";
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
+
+    cout << arr[key - 1];
+
     return 0;
 }
+
+// Q - 3
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+
+//     char text = 'A';
+
+//     int add = 2, i = 32, count = 0;
+
+//     while (count < 13)
+//     {
+//         cout << text << endl;
+//         text = text + i + add;
+//         i = i * (-1);
+//         count++;
+//     }
+
+//     return 0;
+// }
